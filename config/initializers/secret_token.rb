149d7856d55da3311f22cd4972f6b15dcbd37f40
Mark Kremer <mark@socialreferral.com>
2011-11-07 21:31:14 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EchoServiceRails::Application.config.secret_token = '2671fa360c608a980c5e4faf6ee3c8e296132048e978762a937264238bd2eabf16bebca203a82bf6bf2d406bf6efe783d410c997e576b83df162c1d431aebc67'
