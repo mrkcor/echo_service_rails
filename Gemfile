49be47320274c97925a34b2a190f5a0d4cacc178
Mark Cornelissen <mark@boxture.com>
2020-08-22 13:15:58 +0200

    Update Rails to 5.2

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'http://rubygems.org'
 
-gem 'rails', '~> 5.0.0'
+gem 'rails', '~> 5.2'
 gem 'sqlite3', '~> 1.3.5'
 
 gem 'jquery-rails'
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,5 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-EchoServiceRails::Application.config.secret_token = '2671fa360c608a980c5e4faf6ee3c8e296132048e978762a937264238bd2eabf16bebca203a82bf6bf2d406bf6efe783d410c997e576b83df162c1d431aebc67'
 EchoServiceRails::Application.config.secret_key_base = 'aad4f39fc4faabbfcfe1545aaabf16188654a013fb2dae3f5d642028ff0877c5f83f1afe01cf49276844090b2c06b0730c6350ff1e6b4d472e9fbf54a85e4736'
